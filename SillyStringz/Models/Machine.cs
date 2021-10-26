ad19034c06d4ec1c4594acb1da516d13cc2b93d4
William Sercombe <wsercombe@gmail.com>
2021-10-25 17:09:28 -0700

    Set up machine model

---
+++ SillyStringz/Models/Machine.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+// machine = student
+namespace SillyStringz.Models
+
+{
+  public class Machine
+  {
+    public Machine()
+    {
+      this.JoinEntities = new HashSet<EngineerMachine>();
+    }
+
+    public int MachineId { get; set; }
+    [DisplayName("Machine Name")]
+    [Required(ErrorMessage = "Machine name is required")]
+    public string MachineName { get; set; }
+    [DisplayName("Machine Details")]
+    [Required(ErrorMessage = "Machine Details are required")]
+    public DateTime MachineDetails {get; set;}
+
+    public virtual ICollection<EngineerMachine> JoinEntities { get;}
+  }
+}
